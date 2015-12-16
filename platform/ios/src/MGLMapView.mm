ca803cb8895d4393c49e46e83f9af510908d31b0
Romain Quidet <romain.quidet.ext@mappy.com>
2015-12-16 16:34:55 +0100

    [ios] updated callout view protocol, adding sources to gpyi

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -2499,7 +2499,8 @@ std::chrono::steady_clock::duration MGLDurationInSeconds(float duration)
         {
             self.calloutViewForSelectedAnnotation = [self.delegate mapView:self customCalloutViewForAnnotation:annotation];
         }
-        else {
+        if (!self.calloutViewForSelectedAnnotation)
+        {
             self.calloutViewForSelectedAnnotation = [self calloutViewForAnnotation:annotation];
         }
 
