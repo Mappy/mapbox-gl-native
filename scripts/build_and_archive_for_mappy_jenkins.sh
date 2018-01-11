cac07cb43f784438f1e63f6a62b844bef9778af7
Mathieu <mathieu.ouvrard.ext@mappy.com>
2018-01-11 14:28:19 +0100

    IOMA-7543: parametrize script to enable static or dynamic build

--- scripts/build_and_archive_for_mappy_jenkins.sh
+++ scripts/build_and_archive_for_mappy_jenkins.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+# build_and_archive_for_mappy_jenkins - A script to build mapboxgl in release mode and archive on Mappy's build server.
+
 export LC_ALL=en_US.UTF-8
+
+MSG="Required argument should either be 'static' or 'dynamic'"
+if [ $# != 1 ]; then
+    echo $MSG
+    exit
+elif [[ $1 != "static" && $1 != "dynamic" ]]; then
+    echo $MSG
+    exit
+fi 
 
 git submodule init
 git submodule update
 make clean && make distclean
-make ipackage-strip FORMAT=static BUILDTYPE=Release
+make ipackage-strip FORMAT=$1 BUILDTYPE=Release
 
-cd build/ios/pkg
-mv static/Mapbox.framework mapbox-ios-sdk
-cp -r static/Mapbox.bundle/* mapbox-ios-sdk
-tar -zcf mapbox-ios-sdk.tar.gz mapbox-ios-sdk
+if [[ $1 = "static" ]]; then
+	cd build/ios/pkg
+	mv static/Mapbox.framework mapbox-ios-sdk
+	cp -r static/Mapbox.bundle/* mapbox-ios-sdk
+	tar -zcf mapbox-ios-sdk-static.tar.gz mapbox-ios-sdk
+elif [[ $1 = "dynamic" ]]; then
+	cd build/ios
+	mv pkg mapbox-ios-sdk
+	tar -zcf mapbox-ios-sdk-dynamic.tar.gz mapbox-ios-sdk
+fi 
+
