0b793667ccbd43770c726f387e5fa85747cf6481
David Silvera <david.silvera@mappy.com>
2018-08-08 12:04:31 +0200

    Fix merge EGLConfigChooser

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/renderer/egl/EGLConfigChooser.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/renderer/egl/EGLConfigChooser.java
@@ -288,21 +288,7 @@ public class EGLConfigChooser implements GLSurfaceView.EGLConfigChooser {
    * Detect if we are in emulator.
    */
   private boolean inEmulator() {
-    return System.getProperty("ro.kernel.qemu") != null || isGenymotionEmulator()|| buildModelContainsEmulatorHints();
-  }
-
-  private boolean isGenymotionEmulator() {
-    String buildManufacturer = Build.MANUFACTURER;
-    return buildManufacturer != null &&
-            (buildManufacturer.contains("Genymotion") || buildManufacturer.equals("unknown"));
-  }
-
-  private boolean buildModelContainsEmulatorHints() {
-    String buildModel = Build.MODEL;
-    return buildModel.startsWith("sdk")
-            || "google_sdk".equals(buildModel)
-            || buildModel.contains("Emulator")
-            || buildModel.contains("Android SDK");
+    return System.getProperty("ro.kernel.qemu") != null;
   }
 
   /**
