05a91cf0213345a51b7d72e5d45edcedde5e9ac9
Jean-Baptiste Quesney <jean.baptiste.quesney.ext@mappy.com>
2020-09-17 14:41:00 +0200

    IOMA-10058: Use iOS 13.7 for build & unit tests.

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-SIMULATOR_VERSION=12.2
+SIMULATOR_VERSION=13.7
 
 xcodebuild clean build -scheme "Example (Swift)" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
-
--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-SIMULATOR_VERSION=12.2
+SIMULATOR_VERSION=13.7
 
 xcodebuild test -scheme "MapboxDirections iOS" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty -r junit --output test-reports/TEST-MBDirections.xml && exit ${PIPESTATUS[0]}
-
