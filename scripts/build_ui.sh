66a68c72503ca6e419a759360493b6f090caeecd
Jean-Baptiste Quesney <jean.baptiste.quesney.ext@mappy.com>
2020-09-17 14:53:01 +0200

    IOMA-10058: Specify simulator language & region when running unit tests.

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -4,4 +4,4 @@ set -e
 
 SIMULATOR_VERSION=13.7
 
-xcodebuild clean build -scheme "Example (Swift)" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty && exit ${PIPESTATUS[0]}
+xcodebuild clean build -scheme "Example (Swift)" | xcpretty && exit ${PIPESTATUS[0]}
--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -4,4 +4,4 @@ set -e
 
 SIMULATOR_VERSION=13.7
 
-xcodebuild test -scheme "MapboxDirections iOS" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" | xcpretty -r junit --output test-reports/TEST-MBDirections.xml && exit ${PIPESTATUS[0]}
+xcodebuild test -scheme "MapboxDirections iOS" -destination "platform=iOS Simulator,name=iPhone 8,OS=${SIMULATOR_VERSION}" -testLanguage fr -testRegion fr_FR | xcpretty -r junit --output test-reports/TEST-MBDirections.xml && exit ${PIPESTATUS[0]}
